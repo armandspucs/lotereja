0f5d3409ca19b98213141d6814c51bbe9b9ede73
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-10-21 12:35:18 +0300

    viss

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert(1)
+const vardi = ['Jana Krūmiņa','Jānis Bērziņš','pēteris koks','līga priede', 'maija liepa'];
+const balvas = ['Ledusskapis','fotoaparāts','Kafijas automāts','gludeklis','Personālais dators','Tosteris','televizors']
+const naudaKopa = 1000000;
+
+let uzvaretajuSkaits = 3;
+
+let rindas = document.querySelector('.rindas');
+//rindas.innerHTML = 'hello';
+
+
+
+function randFunc(maxCipars)
+{
+  let random1 = Math.floor( Math.random() * maxCipars );
+  return random1;
+}
+
+function izlozet()
+{
+    rindas.innerHTML = '';
+    
+    for(let i=0; i < uzvaretajuSkaits; i++) 
+    {
+        let rand = randFunc(vardi.length);
+
+        let uzvaretajs = vardi[rand];
+
+        rindas.innerHTML += `
+        <tr>
+        <td>${i+1}</td>
+        <td>${uzvaretajs}</td>
+        </tr>
+        `;
+    }
+}//beidzas izlozet()
